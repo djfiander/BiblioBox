4b18008a0ddf4191aaf0cc0f20b14c7d32b47a25
David J. Fiander <david@fiander.info>
2012-05-22 20:29:18 -0400

    Convert to using Mako templating

--- root/www/sites/librarybox/scripts/catalogue.py
+++ root/www/sites/librarybox/scripts/catalogue.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
 
-from cgi import escape
-import sys, os
+from mako.template import Template
+from mako.lookup import TemplateLookup
+
 from flup.server.fcgi import WSGIServer
+
+catlookup = TemplateLookup(directories=['/var/www/librarybox/templates'],
+                           module_directory='/tmp/librarybox_modules')
+
+def serve_template(templatename, **kwargs):
+    mytemplate = catlookup.get_template(templatename)
+    return mytemplate.render(**kwargs)
 
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
-    
-    yield '<h1>FastCGI Environment</h1>'
-    yield '<table>'
-    for k, v in sorted(environ.items()):
-	yield '<tr><th>%s</th><td>%s</td></tr>' % (escape(k), escape(v))
-    yield '</table>'
-                             
-WSGIServer(app, bindAddress=("127.0.0.1", 2005)).run()
+    yield serve_template("catalogue.tmpl", environ=environ)
+
+if __name__ == '__main__':
+    WSGIServer(app, bindAddress=("127.0.0.1", 2005)).run()
