6a9651ef93da47a38acabca8d3f4013f045ad43b
David J. Fiander <david@fiander.info>
2012-07-23 12:38:19 -0400

    initial version of template & WSGI based system

--- root/www/sites/librarybox/scripts/catalogue.py
+++ root/www/sites/librarybox/scripts/catalogue.py
@@ -5,7 +5,7 @@ from mako.lookup import TemplateLookup
 
 from flup.server.fcgi import WSGIServer
 
-catlookup = TemplateLookup(directories=['/var/www/librarybox/templates'],
+catlookup = TemplateLookup(directories=['/www/sites/librarybox/templates'],
                            module_directory='/tmp/librarybox_modules')
 
 def serve_template(templatename, **kwargs):
@@ -13,8 +13,11 @@ def serve_template(templatename, **kwargs):
     return mytemplate.render(**kwargs)
 
 def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    yield serve_template("catalogue.tmpl", environ=environ)
+    if ('QUERY_STRING' in environ) and (environ['QUERY_STRING'] == 'root.xml'):
+        start_response('200 OK', [('Content-Type', 'application/atom+xml;profile=opds-catalog;kind=navigation')])
+    else:
+        start_response('200 OK', [('Content-Type', 'text/html')])
+    yield str(serve_template("catalogue.tmpl", environ=environ))
 
 if __name__ == '__main__':
     WSGIServer(app, bindAddress=("127.0.0.1", 2005)).run()
