037eb8ff8e28940e9d7db72321d29e81975bb57b
David J. Fiander <david@fiander.info>
2012-07-24 11:27:23 -0400

    Start to create hierarchy of catalogue feeds

--- root/www/sites/librarybox/scripts/catalogue.py
+++ root/www/sites/librarybox/scripts/catalogue.py
@@ -8,16 +8,23 @@ from flup.server.fcgi import WSGIServer
 catlookup = TemplateLookup(directories=['/www/sites/librarybox/templates'],
                            module_directory='/tmp/librarybox_modules')
 
-def serve_template(templatename, **kwargs):
-    mytemplate = catlookup.get_template(templatename)
-    return mytemplate.render(**kwargs)
+kindmap = {
+    "root.xml" : "navigation",
+    "title.xml": "acquisition",
+    "author.xml": "navigation"
+}
 
 def app(environ, start_response):
-    if ('QUERY_STRING' in environ) and (environ['QUERY_STRING'] == 'root.xml'):
-        start_response('200 OK', [('Content-Type', 'application/atom+xml;profile=opds-catalog;kind=navigation')])
+    if 'QUERY_STRING' in environ:
+        qs = environ['QUERY_STRING']
+
+        templ = catlookup.get_template(qs)
+        ctype = 'application/atom+xml;profile=opds-catalog;kind={}'.format(kindmap[qs])
+
+        start_response('200 OK', [('Content-Type', ctype)])
+        yield str(templ.render(environ=environ))
     else:
         start_response('200 OK', [('Content-Type', 'text/html')])
-    yield str(serve_template("catalogue.tmpl", environ=environ))
 
 if __name__ == '__main__':
     WSGIServer(app, bindAddress=("127.0.0.1", 2005)).run()
