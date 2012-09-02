65a3aa61ee3f684086af9482df17d80d66cef65a
David J. Fiander <david@fiander.info>
2012-09-02 11:45:48 -0400

    Clean up template rendering; experiment with RESTful URIs for books

--- root/www/sites/librarybox/scripts/opds.py
+++ root/www/sites/librarybox/scripts/opds.py
@@ -5,10 +5,11 @@
 #
 
 from sys import stderr
+import urllib
 
 import bottle
 from bottle import route
-from catutils import catlookup, catrender
+from catutils import catdb, catrender
 
 opds_acq = "application/atom+xml;profile=opds-catalog;kind=acquisition"
 opds_nav = "application/atom+xml;profile=opds-catalog;kind=navigation"
@@ -17,15 +18,32 @@ opds_nav = "application/atom+xml;profile=opds-catalog;kind=navigation"
 def index():
     stderr.write("index!\n")
     bottle.response.set_header('Content-Type', opds_nav)
-    template = catlookup.get_template("root.xml")
-    return catrender(template)
+    return catrender("root.xml")
 
 @route('/title')
 def title():
     stderr.write("title!\n")
     bottle.response.set_header('Content-Type', opds_acq)
-    template = catlookup.get_template("title.xml")
-    return catrender(template)
+    return catrender("title.xml")
+
+@route('/title/<id>/<fmt>')
+def fetch_title(id, fmt):
+    qparm = (id, fmt)
+    conn = catdb()
+    try:
+        cur = conn.execute("""SELECT b.filename as fname
+                              FROM books b WHERE b.work_id = ?
+                                           AND b.format_id = ?""", qparm)
+        fname = cur.fetchone()
+        if fname is None:
+            bottle.abort(404, 'No such book')
+        else:
+            # fname is actually a sqlite Row object
+            bottle.redirect(urllib.quote(fname['fname']))
+    finally:
+        conn.close()
+    # Both abort() and redirect() raise HTTP exceptions, so this
+    # will never return normally
 
 @route('/author')
 @route('/author/<au>')
@@ -36,10 +54,10 @@ def author(au=None):
 
     if au:
         bottle.response.set_header('Content-Type', opds_acq)
-        template = catlookup.get_template("author.xml")
+        template = "author.xml"
     else:
         bottle.response.set_header('Content-Type', opds_nav)
-        template = catlookup.get_template("authorbrowse.xml")
+        template = "authorbrowse.xml"
         
     return(catrender(template, author_id=au))
 
