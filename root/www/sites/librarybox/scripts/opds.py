4923d3779a7c6a57b0c715fb60039ba0c917e208
David J. Fiander <david@fiander.info>
2012-09-02 14:32:17 -0400

    Convert to using the Bottl SQLite3 plugin
    This plugin handles opening and closing DB connections and
    trapping exceptions to ensure that transactions are committed or
    rolled back, and that the database connection is properly
    terminated at the end of a request.

--- root/www/sites/librarybox/scripts/opds.py
+++ root/www/sites/librarybox/scripts/opds.py
@@ -9,70 +9,69 @@ import urllib
 
 import bottle
 from bottle import route
+from bottle_sqlite import SQLitePlugin
 
 from catutils import catdb, catrender
 import queries
 
-opds_acq = "application/atom+xml;profile=opds-catalog;kind=acquisition"
-opds_nav = "application/atom+xml;profile=opds-catalog;kind=navigation"
+opds_acq = 'application/atom+xml;profile=opds-catalog;kind=acquisition'
+opds_nav = 'application/atom+xml;profile=opds-catalog;kind=navigation'
+
+import socket
+
+if socket.gethostname().lower() == 'librarybox':
+    stderr.write('Opening site database\n')
+    dbfile = '/www/sites/librarybox/data/librarybox.db'
+else:
+    stderr.write('Opening local database\n')
+    dbfile = './data/librarybox.db'
+
+# This magically creates a db connection for those routes that
+# have a parameter named 'db'
+bottle.install(SQLitePlugin(dbfile))
 
 @route('/')
 def index():
-    stderr.write("index!\n")
+    stderr.write('index!\n')
     bottle.response.set_header('Content-Type', opds_nav)
-    return catrender("root.xml")
+    return catrender('root.xml')
 
 @route('/title')
-def title():
-    stderr.write("title!\n")
+def title(db):
+    stderr.write('title!\n')
     bottle.response.set_header('Content-Type', opds_acq)
-    conn = catdb()
-    try:
-        return catrender("title.xml", cursor = queries.titlelist(conn))
-    finally:
-        conn.close()
+    return catrender('title.xml', cursor = queries.titlelist(db))
 
 @route('/title/<id>/<fmt>')
-def fetch_title(id, fmt):
+def fetch_title(db, id, fmt):
     qparm = (id, fmt)
-    conn = catdb()
-    try:
-        cur = conn.execute("""SELECT b.filename as fname
-                              FROM books b WHERE b.work_id = ?
-                                           AND b.format_id = ?""", qparm)
-        fname = cur.fetchone()
-        if fname is None:
-            bottle.abort(404, 'No such book')
-        else:
-            # fname is actually a sqlite Row object
-            bottle.redirect(urllib.quote(fname['fname']))
-    finally:
-        conn.close()
+    cur = db.execute('''SELECT b.filename as fname
+                        FROM books b WHERE b.work_id = ?
+                                     AND b.format_id = ?''', qparm)
+    fname = cur.fetchone()
+    if fname is None:
+        bottle.abort(404, 'No such book')
+    else:
+        # fname is actually a sqlite Row object
+        bottle.redirect(urllib.quote(fname['fname']))
+
     # Both abort() and redirect() raise HTTP exceptions, so this
     # will never return normally
 
 @route('/author')
-@route('/author/<au>')
-def author(au=None):
-    stderr.write("author!\n")
-    if 'au' in bottle.request.params:
-        au = bottle.request.params['au']
+def authorlist(db):
+    stderr.write('browse author!\n')
+    bottle.response.set_header('Content-Type', opds_nav)
+    cur = queries.author_list(db)
+    return(catrender('authorbrowse.xml', cursor=cur))
 
-    conn = catdb()
-    try:
-        if au:
-            bottle.response.set_header('Content-Type', opds_acq)
-            template = "author.xml"
-            author_info = queries.author_info(conn, au)
-            if author_info is None:
-                bottle.abort(404, 'Unknown author id')
-            cur = queries.author_works(conn, au)
-        else:
-            bottle.response.set_header('Content-Type', opds_nav)
-            template = "authorbrowse.xml"
-            author_info = None
-            cur = queries.author_list(conn)
-        return(catrender(template, cursor=cur, author_id=au,
+@route('/author/<au>')
+def author(db, au=None):
+    stderr.write('author!\n')
+    bottle.response.set_header('Content-Type', opds_acq)
+    author_info = queries.author_info(db, au)
+    if author_info is None:
+        bottle.abort(404, 'Unknown author id')
+    cur = queries.author_works(db, au)
+    return(catrender('author.xml', cursor=cur, author_id=au,
                          author_info=author_info))
-    finally:
-        conn.close()
